17b9638d45b42c979262ea7a395fa00392a62890
Alex Le <alexle512@gmail.com>
2018-10-30 15:23:56 -0500

    Travel App

--- index.js
+++ index.js
@@ -1,61 +1,46 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var app = express()
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-
-
-//the task array with initial placeholders for added task
-var task = ["buy socks", "practice with nodejs"]
-//placeholder for removed task
-var complete = ["finish nodejs"]
-
-
-//post route for adding new task
-app.post('/addtask', function (req, res){
-    var newTask = req.body.newTask
-//add the new task from the post route into the array
-    task.push(newTask)
-//after adding to the array go back to the roote route
-    res.redirect("/")
-})
-
-app.post("/removetask", function(req,res){
-    var completeTask = req.body.check;
-    if (typeof completeTask === "string"){
-        complete.push(completeTask)
-        task.splice(task.indexOf(completeTask), 1)
-    } else if (typeof completeTask === "object"){
-        for (var i = 0; i<completeTask.length; i++){
-            complete.push(completeTask[i])
-            task.splice(task.indexOf(compelteTask[i]), 1)
-        }
-    }
-    res.redirect("/")
-})
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
 
 
 
-//render the ejs and display added task, completed task
-app.get('/', function (req,res){
-    res.render('index', {task: task , complete: complete})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var destination = ["Denver", "Boston"];
+var complete = ["Seattle"];
+
+
+//post route for adding new destination 
+app.post("/adddestination", function(req, res) {
+    var newdestination = req.body.newdestination;
+    //add the new destination from the post route
+    destination.push(newdestination);
+    res.redirect("/");
 });
 
+app.post("/removedestination", function(req, res) {
+    var completedestination = req.body.check;
+    //check for the "typeof" the different completed destination, then add into the complete destination
+    if (typeof completedestination === "string") {
+        complete.push(completedestination);
+        //check if the completed destination already exits in the destination when checked, then remove it
+        destination.splice(destination.indexOf(completedestination), 1);
+    } else if (typeof completedestination === "object") {
+        for (var i = 0; i < completedestination.length; i++) {
+            complete.push(completedestination[i]);
+            destination.splice(destination.indexOf(completedestination[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
 
-app.listen(3000, function(){
-    console.log("Example app listening on port 3000!")
-})
+app.get("/", function(req, res) {
+    res.render("index", { destination: destination, complete: complete });
+});
 
-
-
-
-//CORS ISSUE 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
+app.listen(3000, function() {
+    console.log("server is running on port 3000");
+});
